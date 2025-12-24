0016c632d57abdee3f35030b62eaff89f6ec0477
Sarah Jameson <sarah.jameson@sarah.jameson-JWYX69RW9N>
2025-12-24 23:40:46 +0000

    Initial commit - Eleventy weather project

---
+++ .eleventy.js
@@ -0,0 +1,8 @@
+// const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("js");
+  eleventyConfig.addPassthroughCopy("images");
+//   eleventyConfig.addPlugin(eleventyNavigationPlugin);
+};
